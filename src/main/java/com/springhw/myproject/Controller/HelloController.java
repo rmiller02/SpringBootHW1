1d3ee0f2d5229a359e5e375d64430038fa9c4ec6
rmiller02 <r_miller02@yahoo.com>
2020-06-22 07:41:51 -0400

    first commit

---
+++ src/main/java/com/springhw/myproject/Controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.springhw.myproject.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "Hello There";
+    }
+
+    @RequestMapping("/home")
+    public String home(){
+        return "index.html";
+    }
+    
+    @RequestMapping("/form")
+    public String form(){
+        return "form.html";
+    }
+
+}  
